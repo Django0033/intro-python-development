3a0af09f5d3c7203a3568f49792a6e39132e1b22
Django0033 <edpu0033@gmail.com>
2021-10-22 19:32:22 -0500

    Adding some spaces to make the code more readable

--- amusement-park-rides.py
+++ amusement-park-rides.py
@@ -18,26 +18,34 @@ partner = input('Is there a second rider (yes/no)? ').lower()
 
 if partner == 'yes':
     second_rider.age = int(input('What is the age of the second rider? '))
+
     if second_rider.age >= 12 and second_rider.age <= 17:
         golden_pass2 = input('Do you have a golden passport (yes/no)? ').lower()
         if golden_pass2 == 'yes':
             second_rider.age = 18
+
     second_rider.height = int(input('What is the height of the second rider? '))
+
     if (first_rider.age >= 18 or second_rider.age >= 18) and \
             (first_rider.height >= 36 and second_rider.height >= 36):
         access_granted = True
+
     elif first_rider.age >= 12 and second_rider.age >= 12 and \
             first_rider.height >= 52 and second_rider.height >= 52:
         access_granted = True
+
     elif ((first_rider.age >= 14 and second_rider.age >= 16) or \
             (first_rider.age >= 16 and second_rider.age >= 14)) and \
                 (first_rider.height >= 36 and second_rider.height >= 36):
         access_granted = True
+
     else:
         access_granted = False
+
 else:
     if first_rider.age >= 18 and first_rider.height >= 62:
         access_granted = True
+
     else:
         access_granted = False
 
