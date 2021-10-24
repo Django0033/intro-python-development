d67d04e566b2bae3a54ea0943525c080021a5fe4
Django0033 <edpu0033@gmail.com>
2021-10-24 12:30:44 -0500

    Added examples.

--- examples/loops.py
+++ examples/loops.py
@@ -6,3 +6,18 @@ while tip < 0:
     # Jump back up to line 3
 
 print(f'You have tipped an amount of ${tip:.2f}')
+
+# The variable must be initialized with a value that access the loop
+# The variables must be declared outside the loop
+# The standard initialization value is 0
+number = 0
+
+# Keep looping as long as the number is less than or equal to 5
+while number <= 5:
+    # Display the number
+    print(f'The number is: {number}')
+
+    #Update the number to be one more than it was
+    number = number + 1
+
+print('Finished with the loop')
