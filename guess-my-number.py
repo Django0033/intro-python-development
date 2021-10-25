728c3429da7ba6ce3ec07c6b717bf9972b91e253
Django0033 <edpu0033@gmail.com>
2021-10-25 02:16:52 -0500

    Adding guess-my-number.py and loops-checkpoint.py

---
+++ guess-my-number.py
@@ -0,0 +1,17 @@
+import random
+
+magic_number = random.randint(1, 100)
+guess = int(input('What is your guess? '))
+guess_count = 1
+
+while magic_number != guess:
+    if guess < magic_number:
+        print('Higher')
+        guess = int(input('What is your guess? '))
+        guess_count = guess_count + 1
+    elif guess > magic_number:
+        print('Lower')
+        guess = int(input('What is your guess? '))
+        guess_count = guess_count + 1
+
+print(f'You guessed it after {guess_count} trys!')
