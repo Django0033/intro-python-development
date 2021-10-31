9108d14ddb1acbba1954bd904cfaf3061364d2f2
Django0033 <edpu0033@gmail.com>
2021-10-30 23:59:05 -0500

    Deleted an unused variable.

--- img-project/working-with-pixels.py
+++ img-project/working-with-pixels.py
@@ -3,7 +3,6 @@ from PIL import Image
 image_original = Image.open('./img/beach.jpg') # Loading the img
 pixels_original = image_original.load() # Getting pixels info
 pixels = [pixels_original[100, 200], pixels_original[101, 200], pixels_original[102, 200],pixels_original[103, 200],pixels_original[104, 200]] # List of pixels to work with
-number_pixel = 0
 
 image_original.show() # Opens a window to show an image
 
