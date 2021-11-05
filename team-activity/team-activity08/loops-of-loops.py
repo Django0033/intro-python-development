8d52baf5a06ddc17fc6dae98b4ce8c0a03184503
Django0033 <edpu0033@gmail.com>
2021-11-04 23:34:24 -0500

    Changed the way of getting the highest product

--- team-activity/team-activity08/loops-of-loops.py
+++ team-activity/team-activity08/loops-of-loops.py
@@ -3,11 +3,12 @@ number_rows_columns = int(input('How many columns and rows do you want in your m
 # When we use range () in the loop below, it will go ug to, but NOT INCLUDING the number
 # so here we set the rane_size to be the user's number_rows_columns + 1
 range_size = number_rows_columns + 1
+product = number_rows_columns * number_rows_columns # Gets the highest product
 
-for i in range(1, range_size): # This creates rows
+# for i in range(1, range_size): # This creates rows
 
-    for j in range(1 , range_size): # This prints each number of a row
-        product = i * j
+#     for j in range(1 , range_size): # This prints each number of a row
+#         product = i * j
 
 product_digits = len(str(product)) # This gets the number of digits of the last product
 
