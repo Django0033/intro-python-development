6a03469f6010cdc16f338bda487262df21d34998
Django0033 <edpu0033@gmail.com>
2021-11-07 23:27:07 -0500

    Added the menu and quit function.

--- shopping-cart.py
+++ shopping-cart.py
@@ -1,2 +1,14 @@
+menu = ['1. Add item', '2. View cart', '3. Remove item', '4. Compute total', '5. Quit']
+action = 0
+
 print()
 print('Welcome to the Shopping Cart Program!')
+
+while action != 5:
+    print()
+    print('Please select one of the following:')
+    
+    for menu_item in menu:
+        print(menu_item)
+
+    action = int(input('Please enter an action: '))
