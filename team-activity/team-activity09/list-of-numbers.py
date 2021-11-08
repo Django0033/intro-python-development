ab2612bb9e5ff585de90bb07ec3db9ebabbcb8be
Django0033 <edpu0033@gmail.com>
2021-11-07 22:40:50 -0500

    Added sorted list, finished team activity.

--- team-activity/team-activity09/list-of-numbers.py
+++ team-activity/team-activity09/list-of-numbers.py
@@ -3,6 +3,7 @@ number = ''
 number_sum = 0
 positive_numbers = []
 
+print()
 print('Enter a list of numbers, type 0 when finished.')
 
 while number != 0:
@@ -23,3 +24,10 @@ print(f'The sum is: {int(number_sum)}')
 print(f'The average is: {number_average}')
 print(f'The largest number is: {max(numbers)}')
 print(f'The smallest positive number is: {int(min(positive_numbers))}')
+
+numbers.sort()
+
+print('The sorted list is:')
+
+for number in numbers:
+    print(int(number))
