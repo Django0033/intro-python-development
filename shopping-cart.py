259ea4a191804eb94c581013f5b278cbde6bb29a
Django0033 <edpu0033@gmail.com>
2021-11-07 23:36:56 -0500

    Implemented the 'add item' function.

--- shopping-cart.py
+++ shopping-cart.py
@@ -1,5 +1,6 @@
 menu = ['1. Add item', '2. View cart', '3. Remove item', '4. Compute total', '5. Quit']
 action = 0
+shopping_cart = []
 
 print()
 print('Welcome to the Shopping Cart Program!')
@@ -12,3 +13,8 @@ while action != 5:
         print(menu_item)
 
     action = int(input('Please enter an action: '))
+
+    if action == 1:
+        item = input('What item would you like to add? ')
+        shopping_cart.append(item)
+        print(f'\'{item}\' has been added to the cart.')
