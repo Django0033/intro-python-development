c56ef45b9aa4ece7a11179800bfda22d7b6856bc
Django0033 <edpu0033@gmail.com>
2021-11-29 02:16:16 -0500

    Cleaning code

--- team-activity/team-activity13/areas-of-shapes-revisited.py
+++ team-activity/team-activity13/areas-of-shapes-revisited.py
@@ -1,14 +1,23 @@
+import math
+
+def compute_area_rectangle(length, width):
+    return length * width
+
 def compute_area_square(side):
     return side ** 2
+
+def comute_area_circle(radius):
+    return math.pi * radius ** 2
 
 square_side = int(input('Enter the length of a side of a square: '))
 square_area = compute_area_square(square_side)
 print('The area of the square is: {}'.format(square_area))
 
-def compute_area_rectangle(length, width):
-    return length * width
-
 rectangle_length = int(input('Enter the length of a rectangle: '))
 rectangle_width = int(input('Enter the width of a rectangle: '))
 rectangle_area = compute_area_rectangle(rectangle_length, rectangle_width)
 print('The area of the rectangle is: {}'.format(rectangle_area))
+
+circle_radius = int(input('Enter the radius of a circle: '))
+circle_area = comute_area_circle(circle_radius)
+print('The area of the circle is: {}'.format(circle_area))
