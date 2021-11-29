539baae2a6599503cb27b94c9be339b57aeef602
Django0033 <edpu0033@gmail.com>
2021-11-29 02:07:48 -0500

    Added the compute_area_rectangle func

--- team-activity/team-activity13/areas-of-shapes-revisited.py
+++ team-activity/team-activity13/areas-of-shapes-revisited.py
@@ -4,3 +4,11 @@ def compute_area_square(side):
 square_side = int(input('Enter the length of a side of a square: '))
 square_area = compute_area_square(square_side)
 print('The area of the square is: {}'.format(square_area))
+
+def compute_area_rectangle(length, width):
+    return length * width
+
+rectangle_length = int(input('Enter the length of a rectangle: '))
+rectangle_width = int(input('Enter the width of a rectangle: '))
+rectangle_area = compute_area_rectangle(rectangle_length, rectangle_width)
+print('The area of the rectangle is: {}'.format(rectangle_area))
