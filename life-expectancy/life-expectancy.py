4f0c765b84cd181aa50b7d076df48d2d2565a8ac
Django0033 <edpu0033@gmail.com>
2021-11-30 19:03:24 -0500

    Cleaning life-expectancy.py

--- life-expectancy/life-expectancy.py
+++ life-expectancy/life-expectancy.py
@@ -34,11 +34,13 @@ abbreviations_of_interest = []
 years_of_interest = []
 life_of_interest = []
 
+
 def fill_lists_of_interest():
     countries_of_interest.append(countries[i])
     abbreviations_of_interest.append(abbreviations[i])
     years_of_interest.append(years[i])
     life_of_interest.append(float(life_expectancies[i]))
+
 
 year_of_interest = input('\nEnter the year of interest: ')
 print('\nThe overall max life expectancy is: {} from {} in {}'.format(highest_life_expectancy, highest_life_country, highest_life_year) + '\nThe '
@@ -51,13 +53,18 @@ for i in range(len(years)):
 
 average_life_interest = sum(life_of_interest) / len(life_of_interest)
 max_life_interest = max(life_of_interest)
-max_country_interest = countries_of_interest[life_of_interest.index(max_life_interest)]
+max_country_interest = countries_of_interest[life_of_interest.index(
+    max_life_interest)]
 min_life_interest = min(life_of_interest)
-min_country_interest = countries_of_interest[life_of_interest.index(min_life_interest)]
+min_country_interest = countries_of_interest[life_of_interest.index(
+    min_life_interest)]
 
-print('The average life expectancy across all countries was {:.2f}'.format(average_life_interest))
-print('The max life expectancy was in {} with {}'.format(max_country_interest, max_life_interest))
-print('The min life expectancy was in {} with {}'.format(min_country_interest, min_life_interest))
+print('The average life expectancy across all countries was {:.2f}'.format(
+    average_life_interest))
+print('The max life expectancy was in {} with {}'.format(
+    max_country_interest, max_life_interest))
+print('The min life expectancy was in {} with {}'.format(
+    min_country_interest, min_life_interest))
 
 country_of_interest = input('\nEnter the country of interest: ')
 countries_of_interest = []
@@ -71,12 +78,15 @@ for i in range(len(countries)):
 
 average_life_interest = sum(life_of_interest) / len(life_of_interest)
 max_life_interest = max(life_of_interest)
-max_year_interest = years_of_interest[life_of_interest.index(max_life_interest)]
+max_year_interest = years_of_interest[life_of_interest.index(
+    max_life_interest)]
 min_life_interest = min(life_of_interest)
-min_year_interest = years_of_interest[life_of_interest.index(min_life_interest)]
+min_year_interest = years_of_interest[life_of_interest.index(
+    min_life_interest)]
 
 print('\nFor {}:'.format(country_of_interest.capitalize()))
 print('The average life expectancy is {:.2f}'.format(average_life_interest))
-print('The max life expectancy was in {} with {}'.format(max_year_interest, max_life_interest))
-print('The min life expectancy was in {} with {}'.format(min_year_interest, min_life_interest))
-
+print('The max life expectancy was in {} with {}'.format(
+    max_year_interest, max_life_interest))
+print('The min life expectancy was in {} with {}'.format(
+    min_year_interest, min_life_interest))
